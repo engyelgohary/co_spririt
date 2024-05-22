f8b3734b7f449bad6eef279e36de236f918415ef
Your Name <you@example.com>
2024-05-22 09:30:42 +0300

    Menu Screen For all ui Done

--- lib/ui/admin/Menu/menu_screenA.dart
+++ lib/ui/admin/Menu/menu_screenA.dart
@@ -1,6 +1,5 @@
 import 'package:co_spririt/ui/auth/login.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_screenutil/flutter_screenutil.dart';
 import '../../../utils/components/MenuItem.dart';
 import '../../../utils/theme/appColors.dart';
 import '../Notifactions/notifictionsScreen.dart';
@@ -32,28 +31,7 @@ class MenuScreenAdmin extends StatelessWidget {
         ),
       body: Column(
         children: [
-          Container(
-            padding: EdgeInsets.only(top: 25.h),
-            child: ListTile(
-              leading:  CircleAvatar(
-                backgroundColor: AppColor.secondColor,
-                radius:35.r,
-              ),
-              title: Text('Matteo',
-                  style: Theme.of(context)
-                      .textTheme
-                      .titleSmall!
-                      .copyWith(fontSize: 17)),
-              subtitle:  Padding(
-                padding:  EdgeInsets.only(top: 8.h),
-                child: Text('Super Admin',
-                    style: Theme.of(context)
-                        .textTheme
-                        .titleSmall!
-                        .copyWith(fontWeight: FontWeight.w400, fontSize: 12,color: AppColor.borderColor)),
-              ),
-            )
-            ),
+          CustomProfileUser(name: "Ray" ,role: "Admin"),
           CustomMenuCard(name:'Profile' ,onFunction:(){ Navigator.pushNamed(context, LoginScreen.routeName);},),
           CustomMenuCard(name:'Collaborators' ,onFunction:(){ Navigator.pushNamed(context, LoginScreen.routeName);},),
           CustomMenuCard(name:'Notifications' ,onFunction:(){ Navigator.pushNamed(context, NotifactionScreenAdmin.routName);},),

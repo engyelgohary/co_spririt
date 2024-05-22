2198fc5925b57f9d5de7e250e031bf8e2488d080
Your Name <you@example.com>
2024-05-22 08:10:52 +0300

    Menu Screen For all ui Done

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:co_spririt/ui/auth/login.dart';
+import 'package:co_spririt/ui/collaborator/Menu/menu_screenc.dart';
+import 'package:co_spririt/ui/superadmin/Menu/menu_screensa.dart';
 import 'package:co_spririt/ui/superadmin/Notifactions/notifictionsScreen.dart';
 import 'package:co_spririt/utils/theme/appTheme.dart';
 import 'package:flutter/material.dart';
@@ -20,10 +22,11 @@ class MyApp extends StatelessWidget {
       builder: (context, child) {
         return  MaterialApp(
           debugShowCheckedModeBanner: false,
-          initialRoute: LoginScreen.routeName,
+          initialRoute:  MenuScreenSuperAdmin.routeName,
           routes: {
             LoginScreen.routeName: (context) => LoginScreen(),
             NotifactionScreenSuperAdmin.routName: (context) => NotifactionScreenSuperAdmin(),
+            MenuScreenCollaborators.routeName:(context) => MenuScreenCollaborators(),
           },
           theme: AppTheme.mainTheme,
         );

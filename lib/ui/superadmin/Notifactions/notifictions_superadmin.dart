5f69492d8dced4c7af6a4ee9efd7d6ec6dfe660d
Yusuf Alsawah <y.alsayed@nu.edu.eg>
2024-08-03 00:20:29 +0300

    Demo notification

--- lib/ui/superadmin/Notifactions/notifictions_superadmin.dart
+++ lib/ui/superadmin/Notifactions/notifictions_superadmin.dart
@@ -1,3 +1,4 @@
+import 'package:awesome_notifications/awesome_notifications.dart';
 import 'package:co_spririt/utils/components/appbar.dart';
 import 'package:co_spririt/utils/theme/appColors.dart';
 import 'package:flutter/material.dart';
@@ -8,12 +9,10 @@ class NotifactionScreenSuperAdmin extends StatefulWidget {
   const NotifactionScreenSuperAdmin({super.key});
 
   @override
-  State<NotifactionScreenSuperAdmin> createState() =>
-      _NotifactionScreenSuperAdminState();
+  State<NotifactionScreenSuperAdmin> createState() => _NotifactionScreenSuperAdminState();
 }
 
-class _NotifactionScreenSuperAdminState
-    extends State<NotifactionScreenSuperAdmin> {
+class _NotifactionScreenSuperAdminState extends State<NotifactionScreenSuperAdmin> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -29,14 +28,12 @@ class _NotifactionScreenSuperAdminState
         itemBuilder: (context, index) {
           return ListTile(
             leading: const CircleAvatar(
-             backgroundImage: AssetImage("assets/images/photo.png"),
+              backgroundImage: AssetImage("assets/images/photo.png"),
               radius: 25,
             ),
             title: Text('Matteo',
-                style: Theme.of(context)
-                    .textTheme
-                    .titleSmall!
-                    .copyWith(fontWeight: FontWeight.w700)),
+                style:
+                    Theme.of(context).textTheme.titleSmall!.copyWith(fontWeight: FontWeight.w700)),
             subtitle: Column(
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
@@ -48,20 +45,41 @@ class _NotifactionScreenSuperAdminState
                 Text(
                   index % 2 == 0 ? 'End' : 'End Soon',
                   style: TextStyle(
-                    color: index % 2 == 0
-                        ? AppColor.errorColor
-                        : AppColor.secondColor,
+                    color: index % 2 == 0 ? AppColor.errorColor : AppColor.secondColor,
                   ),
                 ),
               ],
             ),
-            trailing:  CircleAvatar(
-              backgroundColor: AppColor.SkyColor,
-              radius: 18.r,
-              child: Icon(
-                Icons.info_outline,
-                color: AppColor.secondColor,
-                size: 20,
+            trailing: InkWell(
+              onTap: () async {
+                await AwesomeNotifications().createNotification(
+                    content: NotificationContent(
+                        id: 16, // -1 is replaced by a random number
+                        channelKey: 'basic_channel',
+                        title: 'Huston! The eagle has landed!',
+                        body: "A small step for a man, but a giant leap to Flutter's community!",
+                        bigPicture:
+                            'https://storage.googleapis.com/cms-storage-bucket/d406c736e7c4c57f5f61.png',
+                        largeIcon: 'https://storage.googleapis.com/cms-storage-bucket/0dbfcc7a59cd1cf16282.png',
+                        notificationLayout: NotificationLayout.BigPicture,
+                        payload: {'notificationId': '1234567890'}),
+                    actionButtons: [
+                      NotificationActionButton(
+                        key: 'DISMISS',
+                        label: 'Dismiss',
+                        actionType: ActionType.DismissAction,
+                        isDangerousOption: true,
+                      )
+                    ]);
+              },
+              child: CircleAvatar(
+                backgroundColor: AppColor.SkyColor,
+                radius: 18.r,
+                child: Icon(
+                  Icons.info_outline,
+                  color: AppColor.secondColor,
+                  size: 20,
+                ),
               ),
             ),
           );

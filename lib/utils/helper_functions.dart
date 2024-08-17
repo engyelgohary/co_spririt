16c50f54c8885c294bd5cc9ea60eac91700583b6
Yusuf Alsawah <y.alsayed@nu.edu.eg>
2024-08-17 15:28:25 +0300

    create signalr class and adminList function

--- lib/utils/helper_functions.dart
+++ lib/utils/helper_functions.dart
@@ -60,37 +60,128 @@ Future<void> collaboratorsList(ApiManager apiManager, LoadingStateNotifier loadi
   loadingNotifier.change();
 }
 
-Future<void> testFunction() async {
+Future<void> adminsList(ApiManager apiManager, LoadingStateNotifier loadingNotifier) async {
   try {
-    print("step1");
+    const storage = FlutterSecureStorage();
+    final token = await storage.read(key: 'token');
 
-    final connection = HubConnectionBuilder()
-        .withUrl(
-            'http://${ApiConstants.baseUrl}/chat',
-            HttpConnectionOptions(
-              logMessageContent: true,
-              logging: (level, message) {
-                print("-level2 = ${level.name}");
-                print("-level = $level");
-                print("-message = $message");
-              },
-            ))
-        .build();
+    if (token == null) {
+      throw Exception('No token found. Please log in.');
+    }
 
-    print("step2");
+    Map<String, dynamic> decodedToken = Jwt.parseJwt(token);
 
-    await connection.start();
+    final collaboratorData =
+        await apiManager.fetchCollaboratorDetails(int.parse(decodedToken['nameid']));
 
-    print("step3");
-
-    connection.on('ReceiveMessageUpdate', (message) {
-      print("potato");
-      print(message);
-      print(message.toString());
-    });
-    print("step4");
+    loadingNotifier.response = <GetAdmin>[
+      await apiManager.fetchAdminDetails(collaboratorData.adminId ?? 0)
+    ];
   } catch (e) {
-    print(e);
+    print("- CollaboratorsList error : $e");
+    loadingNotifier.response = null;
+  }
+  loadingNotifier.change();
+}
+
+class Signalr {
+  Signalr.signalr();
+  static Signalr? _instance;
+
+  factory Signalr() => _instance ??= Signalr.signalr();
+
+  void dispose() {
+    connection.stop();
+    _instance = null;
+  }
+
+  int? senderId;
+  int? receiverId;
+  ScrollController? scrollController;
+  ListNotifier<Message>? listNotifier;
+
+  final connection = HubConnectionBuilder()
+      .withUrl(
+        'http://${ApiConstants.baseUrl}/chat',
+        HttpConnectionOptions(
+          logMessageContent: true,
+          logging: (level, message) {
+            // print("-level = $level");
+            print("-message = $message");
+          },
+        ),
+      )
+      .build();
+
+  Future<void> start() async {
+    if (senderId == null) {
+      const storage = FlutterSecureStorage();
+      final token = await storage.read(key: 'token');
+
+      if (token == null) {
+        throw Exception('No token found. Please log in.');
+      }
+
+      senderId = int.parse(Jwt.parseJwt(token)["nameid"]);
+    }
+
+    try {
+      await connection.start();
+      connection.on('ReceiveMessageUpdate', (message) {
+        if (message != null && message.isNotEmpty) {
+          final incomingMessage = Message(
+            id: message[0]["Message"]["Id"],
+            senderId: message[0]["Message"]["FromId"],
+            receiverId: message[0]["Message"]["ToId"],
+            content: message[0]["Message"]["Content"],
+            sender: false,
+            read: message[0]["Message"]["Read"],
+            senderEmail: message[0]["User"]["Email"],
+            senderFirstName: message[0]["User"]["FirstName"],
+            senderLastName: message[0]["User"]["LastName"],
+          );
+          incomingMessage.parseTime(message[0]["Message"]["Timestamp"]);
+
+          if (senderId == incomingMessage.senderId ||
+              incomingMessage.senderId == incomingMessage.receiverId) {
+            return;
+          } else if (receiverId != incomingMessage.senderId) {
+            AwesomeNotifications().createNotification(
+              content: NotificationContent(
+                id: 16, // -1 is replaced by a random number
+                channelKey: 'basic_channel',
+                title: 'New message from: ${incomingMessage.senderFirstName}',
+                body: incomingMessage.content,
+                notificationLayout: NotificationLayout.BigText,
+              ),
+              actionButtons: [
+                NotificationActionButton(
+                  key: 'DISMISS',
+                  label: 'Dismiss',
+                  actionType: ActionType.DismissAction,
+                  isDangerousOption: true,
+                )
+              ],
+            );
+            print(
+                "Message Notification content: ${incomingMessage.content}\nMessage sender: ${incomingMessage.senderId}\nMessage receiver: ${incomingMessage.receiverId}");
+          } else if (listNotifier != null) {
+            listNotifier!.addItem(incomingMessage);
+            if (scrollController != null) {
+              Future.delayed(
+                const Duration(milliseconds: 300),
+                () => scrollController!.animateTo(scrollController!.position.maxScrollExtent,
+                    duration: const Duration(milliseconds: 300), curve: Curves.easeOut),
+              );
+            }
+            print(
+                "Message List content: ${incomingMessage.content}\nMessage sender: ${incomingMessage.senderId}\nMessage receiver: ${incomingMessage.receiverId}");
+          }
+        }
+      });
+    } catch (e) {
+      print("- signalr error: ${e}");
+    }
   }
 }
 

7d619a2da939053d0e28441f393c0584601dac49
Your Name <you@example.com>
2024-09-08 16:50:31 +0300

    status done

--- lib/ui/admin/collaboratorsforadmin/collaborators_screen.dart
+++ lib/ui/admin/collaboratorsforadmin/collaborators_screen.dart
@@ -209,25 +209,65 @@ class _CollaboratorsAdminScreenState extends State<CollaboratorsAdminScreen> {
       builder: (context) {
         int? selectedStatus;
         return AlertDialog(
-          title: Text('Select Status',
-              style: Theme.of(context).textTheme.titleSmall!.copyWith(fontSize: 20)),
+          title: Text(
+            'Select Status',
+            style: Theme.of(context).textTheme.titleSmall!.copyWith(fontSize: 20),
+          ),
           content: DropdownButton<int>(
             value: selectedStatus,
             onChanged: (int? newValue) {
               selectedStatus = newValue;
             },
-            items: <int>[1, 2, 3].map<DropdownMenuItem<int>>((int value) {
-              return DropdownMenuItem<int>(
-                value: value,
-                child: Text(
-                  value.toString(),
-                  style: Theme.of(context).textTheme.titleSmall!.copyWith(
+            items: [
+              DropdownMenuItem<int>(
+                value: 1,
+                child: Row(
+                  children: [
+                    Icon(Icons.circle, color: Colors.red),
+                    SizedBox(width: 8),
+                    Text(
+                      'Red (1)',
+                      style: Theme.of(context).textTheme.titleSmall!.copyWith(
                         fontSize: 18,
                         color: AppColor.basicColor,
                       ),
+                    ),
+                  ],
                 ),
-              );
-            }).toList(),
+              ),
+              DropdownMenuItem<int>(
+                value: 2,
+                child: Row(
+                  children: [
+                    Icon(Icons.circle, color: Colors.green),
+                    SizedBox(width: 8),
+                    Text(
+                      'Green (2)',
+                      style: Theme.of(context).textTheme.titleSmall!.copyWith(
+                        fontSize: 18,
+                        color: AppColor.basicColor,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              DropdownMenuItem<int>(
+                value: 3,
+                child: Row(
+                  children: [
+                    Icon(Icons.circle, color: Colors.orange),
+                    SizedBox(width: 8),
+                    Text(
+                      'Orange (3)',
+                      style: Theme.of(context).textTheme.titleSmall!.copyWith(
+                        fontSize: 18,
+                        color: AppColor.basicColor,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
           ),
           actions: [
             SizedBox(
@@ -241,10 +281,10 @@ class _CollaboratorsAdminScreenState extends State<CollaboratorsAdminScreen> {
                   child: Text(
                     'Cancel',
                     style: Theme.of(context).textTheme.titleSmall!.copyWith(
-                          fontSize: 16,
-                          color: AppColor.thirdColor,
-                          fontWeight: FontWeight.w400,
-                        ),
+                      fontSize: 16,
+                      color: AppColor.thirdColor,
+                      fontWeight: FontWeight.w400,
+                    ),
                   ),
                 ),
                 style: ElevatedButton.styleFrom(
@@ -270,9 +310,9 @@ class _CollaboratorsAdminScreenState extends State<CollaboratorsAdminScreen> {
                   child: Text(
                     'Set Status',
                     style: Theme.of(context).textTheme.titleSmall!.copyWith(
-                          fontSize: 16,
-                          color: AppColor.whiteColor,
-                        ),
+                      fontSize: 16,
+                      color: AppColor.whiteColor,
+                    ),
                   ),
                 ),
                 style: ElevatedButton.styleFrom(
